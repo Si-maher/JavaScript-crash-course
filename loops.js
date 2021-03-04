413e18e70944378e58c21e969036a7fe1479a988
Simon Maher <simonmaher8307@hotmail.com>
2021-03-04 23:11:10 +0200

    added for loop

---
+++ loops.js
@@ -0,0 +1,24 @@
+
+const todos = [
+    {
+        id : 1,
+        text : 'Take out trash',
+        isCompleted : true
+
+    },
+    {
+        id : 2,
+        text : 'Meeting with Boss',
+        isCompleted : true
+
+    },
+    {
+        id : 3,
+        text : 'Dentist appointment',
+        isCompleted : false
+
+    }
+]
+for(let i = 0; i < 10; i++){
+    console.log(`For loop number ${i}`);
+}
