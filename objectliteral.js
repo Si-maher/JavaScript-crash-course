76f39b29bab538335485a383c2f152bd68feef27
Simon Maher <simonmaher8307@hotmail.com>
2021-03-04 22:58:54 +0200

    added objectliteral, array of objects

---
+++ objectliteral.js
@@ -0,0 +1,39 @@
+// Key value pairs 
+
+const person = {
+    firstName : 'John',
+    lastName : 'Doe',
+    age : 32,
+    hobbies : ['gym', 'running', 'swimming'],
+    address : {
+        street: '1 main street',
+        city : 'Boston',
+        state : 'MA'
+    }
+}
+
+console.log(person.hobbies[1]);
+console.log(person.address.city);
+
+// Array of Objects 
+
+const todos = [
+    {
+        id : 1,
+        text : 'Take out trash',
+        isCompleted : true
+
+    },
+    {
+        id : 2,
+        text : 'Meeting with Boss',
+        isCompleted : true
+
+    },
+    {
+        id : 3,
+        text : 'Dentist appointment',
+        isCompleted : false
+
+    }
+]
