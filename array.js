16f0bc338a1b89aefd164eecba577f4567e02e8d
Simon Maher <simonmaher8307@hotmail.com>
2021-03-04 22:46:16 +0200

    added array methods

---
+++ array.js
@@ -0,0 +1,13 @@
+// Arrays are variable that hold multiple values 
+
+// constructor method 
+const numbers = new Array(1,2,3,4,5)
+console.log(numbers);
+
+const fruits = ['apples', 'pears', 'oranges']
+console.log(fruits[1]);
+fruits.push('mango')
+console.log(fruits);
+fruits.unshift('strawberry')
+fruits.pop()
+console.log(fruits.indexOf('apple'));
