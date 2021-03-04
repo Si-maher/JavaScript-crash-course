98ea76d7c98c6d25f10ae332db1410e6e2727ac5
Simon Maher <simonmaher8307@hotmail.com>
2021-03-04 22:36:42 +0200

    added string methods

--- string.js
+++ string.js
@@ -4,3 +4,13 @@ const age = 41
 // Template literals 
 
 console.log(`My name is ${name} and I am ${age} years young`);
+
+// A method is a function that is associated with an Object. It needs parenthesis 
+// A property does not need parenthesis 
+
+const sentence = 'Only the dead have seen war'
+console.log(sentence.length);
+console.log(sentence.toLocaleUpperCase());
+console.log(sentence.substring(0,5));
+console.log(sentence.split(''));
+console.log(sentence.split(','));
