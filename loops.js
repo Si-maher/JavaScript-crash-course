a562dd17f461d024fae712b055cc2517774b194e
Simon Maher <simonmaher8307@hotmail.com>
2021-03-04 23:27:22 +0200

    added forEach and map method

--- loops.js
+++ loops.js
@@ -22,3 +22,22 @@ const todos = [
 for(let i = 0; i < 10; i++){
     console.log(`For loop number ${i}`);
 }
+
+let j = 0
+while(j < 10){
+    console.log(`WHile loop number ${j}`);
+    j++
+}
+
+for(let todo of todos) {
+    console.log(todo.text);
+}
+
+todos.forEach(function(todo) {
+    console.log(todo.id);
+})
+
+const todoText = todos.map(function(todo) {
+    return todo.isCompleted
+})
+console.log(todoText);
