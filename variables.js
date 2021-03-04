6057fb31b60a245a54064d4b2fac2bad52db3632
Simon Maher <simonmaher8307@hotmail.com>
2021-03-04 22:22:34 +0200

    added variable examples

---
+++ variables.js
@@ -0,0 +1,27 @@
+// Declare a variable: var, let and const
+
+// var has global scope so let and const are reccomended.
+// Let can be reassigned;
+
+let age = 31
+age =32
+
+console.log(age);
+
+// const can not
+
+ const age1 = 30
+//   age1 = 31
+//   wil return error 
+
+// Primitive data typeof, not assigned to memory
+// String, NUmbers, boolean, Null, underfined 
+
+const name = 'John'
+const age2 = 30
+const isCool = true
+const x = null
+const y = undefined
+let z
+
+console.log(typeof name);
