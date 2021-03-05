fd4bb8560e013b155925882fa7c907197853e1a6
Simon Maher <simonmaher8307@hotmail.com>
2021-03-05 14:39:01 +0200

    added html content, form, css file

--- objectOriented.js
+++ objectOriented.js
@@ -1,4 +1,4 @@
-// Constructor function 
+// Constructor function , Pre classes
 
 function Person (firstName, lastName, Dob){
     this.firstName = firstName
@@ -13,6 +13,21 @@ const person3 = new Person('Jez', 'Doe','3.04.1990')
 const person4 = new Person('Dave', 'Doe','3.04.1990')
 
 console.log(person1);
-console.log(person2);
-console.log(person3);
+console.log(person2.firstName);
+console.log(person3.Dob);
 console.log(person4);
+
+// Class function 
+
+class Person5 {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.dob = dob
+    }
+    getFullName() {
+        return `{this.firsName} ${this.lastName}`
+    }
+}
+const person6 = new Person('Bob', 'Dillon', '03111974')
+console.log(person6);
