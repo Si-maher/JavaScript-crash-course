e72e7747d3fa1a330f69402a5bdd386da3c3ea13
Simon Maher <simonmaher8307@hotmail.com>
2021-03-06 10:39:45 +0200

    added event listener,e default, styles to form

--- dom.js
+++ dom.js
@@ -1,3 +1,12 @@
 const ul = document.querySelector('.items')
 ul.firstElementChild.textContent = 'Hello'
 ul.children[1].innerText = 'World'
+
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    document.querySelector('#my-form').style.background ='#ccc'
+    document.querySelector('body').classList.add('bg-dark')
+    document.querySelector('.items').style.color = 'black'
+})
